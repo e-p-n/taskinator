8d766444b268485141ab3b4ebae08356d7889127
Eric Normann <eric.n@me.com>
2020-12-23 00:16:13 -0500

    Add form content to added li and format it

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,24 @@ var buttonEl = document.querySelector("#save-task");
 var taskToDoEl = document.querySelector("#tasks-to-do");
 var formEl = document.querySelector("#task-form");
 
+
 var createTaskHandler = function(event) {
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
     event.preventDefault();
+    //create list item
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+    //give it a class name
+    taskInfoEl.className = "task-info";
+    //add HTML to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     taskToDoEl.appendChild(listItemEl);
 }
 
